ada43a247afa19be9f28b0dc185ddf1c0d3968c2
Lou Foster <lawuana.foster@att.net>
2023-03-28 16:41:31 -0500

    updates

--- queries.sql
+++ queries.sql
@@ -217,13 +217,14 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
 --add gender
 SELECT emp_no,
     first_name,
 last_name,
     gender
-	
-	
 INTO emp_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
