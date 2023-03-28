aa21d7453ee7a4e45f602b3faada2ddbf6e26d3b
Lou Foster <lawuana.foster@att.net>
2023-03-27 21:52:40 -0500

    updates

--- schema.sql
+++ schema.sql
@@ -52,4 +52,56 @@ CREATE TABLE dept_emp (
 );
 
 SELECT * FROM dept_manager;
+--SELECT * FROM place nameof table here;
 
+--determined that anyone born between 1952 and 1955 will begin to retire
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Create another query that will search for only 1952 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+--Create another query that will search for only 1953 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+
+
+--Create another query that will search for only 1954 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+
+--Create another query that will search for only 1955 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+
+--narrow the Search for Retirement Eligibility
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+
+-- Retirement eligibility
+--Select conditions to quier on employees 
+--born between 1952 and 1955, who were also hired between 1985 and 1988
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
